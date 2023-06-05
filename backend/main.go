bc7c627708a206794b70a251d560584b39f3a967
Tyman3413 <tyman3413@gmail.com>
2023-06-06 00:35:22 +0300

    ready-to-go

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-	"net/http"
-	"log"
+	"database/sql"
 	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+
+	_ "github.com/lib/pq"
 )
 
-func main()  {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
+func main() {
+	// Подключение к базе данных PostgreSQL
+	connection := "user=postgres password=admin dbname=booking sslmode=disable"
 
-func handler(w http.ResponceWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	// Открываем соединение с базой данных
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Проверка соединения с базой данных
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successful connection to the database")
+
+	app := fiber.New()
+
+	app.Get("/page", func(c *fiber.Ctx) error {
+		return c.SendString("Working")
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
