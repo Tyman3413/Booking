f1e75f46c7b55879256f99ad21b804451b8c2587
Tyman3413 <tyman3413@gmail.com>
2023-06-12 21:49:10 +0300

    Database Management

--- backend/models/hotel.go
+++ backend/models/hotel.go
@@ -7,7 +7,16 @@ type Hotel struct {
 	Rating      string `json:"rating"`
 	Location    string `json:"location"`
 	Description string `json:"description"`
+	Reviews     string `json:"reviews"`
 	Image       string `json:"image"`
-	UserID      string `json:"userid"`
-	User        User   `json:"user";gorm:"foreignkey:UserID"`
+	RoomID      string `json:"roomid"`
+	Room        Room   `json:"room";gorm:"foreignkey:RoomID"` // Связка с таблицей Номеров отелей
+	CityID      string `json:"cityid"`
+	City        City   `json:"city";gorm:"foreignkey:CityID"` //
+
+	// Скип
+	UserID string `json:"userid"`
+	User   User   `json:"user";gorm:"foreignkey:UserID"`
+
+	// TODO Связать с таблицей Удобства
 }
---
+++ backend/models/room.go
@@ -0,0 +1,15 @@
+package models
+
+type Room struct {
+	ID          uint   `json:"id"`
+	Type        string `json:"type"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	BedPlaces   string `json:"bedplaces"`
+	Places      string `json:"places"`
+	AddPlaces   string `json:"addplaces"`
+	Square      string `json:"square"`
+	Usability   string `json:"usability"`
+	Price       string `json:"price"`
+}
